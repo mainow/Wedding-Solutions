054aac1d73bdf17f11159bb0f3224916f524c360
maino <mainohgz@gmail.com>
2022-08-18 23:16:50 -0400

    Added gift list modal so that mom doesnt get angry

--- src/pages/GiftsPanel.js
+++ src/pages/GiftsPanel.js
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
 import { GiftsManager } from "../GiftsManager";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 function GiftsPanel(props) {
 
     const [bookedGifts, setBookedGifs] = useState();
+    const [giftList, setGiftList] = useState(false);
+    const [showGiftListModal, setShowGiftListModal] = useState(false);
 
     useEffect(() => {
-        GiftsManager.getBookedGifts(setBookedGifs)
+        GiftsManager.getBookedGifts(setBookedGifs);
+        GiftsManager.getGiftList(setGiftList);
     }, []);
-
     
     return (
-        <div className="container">
-            <h1>Panel de resultados</h1>
+        <div className="container py-3">
+            <div className="d-flex justify-content-between">
+                <h3>Panel de resultados<span className="dot-color">.</span></h3>
+                <Button variant="success" onClick={() => setShowGiftListModal(true)} className="rounded">Lista de regalos</Button>
+            </div>
             <table className="table table-stripped">
                 <thead>
                     <tr>
@@ -34,6 +41,32 @@ function GiftsPanel(props) {
                 }
                 </tbody>
             </table>
+            <Modal show={showGiftListModal} size="lg" onHide={() => setShowGiftListModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Lista de regalo</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <td>Regalo</td>
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.keys(giftList).map((key) => <tr key={key}>
+                                <td className={giftList[key].available && "text-secondary"}>{giftList[key].name}</td>
+                            </tr>)}
+                        </tbody>
+                    </table>
+                    <div className="d-flex justify-content-end gap-1">
+                        <Button onClick={() => setShowGiftListModal(false)} variant="secondary">Close</Button>
+                        <Button variant="primary">Save changes</Button>
+                    </div>
+                </Modal.Body>
+
+            </Modal>
         </div>
     );
 }
